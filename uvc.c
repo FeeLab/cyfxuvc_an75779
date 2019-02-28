ff3a0bbe4ddae56d055cd0a58ee0fcbd07cca352
Joe Scherrer <joebob@mit.edu>
2019-02-28 15:15:07 -0500

    got rid of uvc.c edits, check programming daq boards.rtf

--- uvc.c
+++ uvc.c
@@ -278,27 +278,6 @@ CyFxUVCAddHeader (
     }
 }
 
-
-/* Application Error Handler */
-void
-CyFxAppErrorHandler (
-        CyU3PReturnStatus_t apiRetStatus    /* API return status */
-        )
-{
-    /* This function is hit when we have hit a critical application error. This is not
-       expected to happen, and the current implementation of this function does nothing
-       except stay in a loop printing error messages through the UART port.
-
-       This function can be modified to take additional error handling actions such
-       as cycling the USB connection or performing a warm reset.
-     */
-    for (;;)
-    {
-        CyU3PDebugPrint (4, "Error handler...\r\n");
-        CyU3PThreadSleep (1000);
-    }
-}
-
 /* This function performs the operations for a Video Streaming Abort.
    This is called every time there is a USB reset, suspend or disconnect event.
  */
@@ -737,8 +716,6 @@ CyFxUVCApplnInit (void)
 #ifdef UVC_PTZ_SUPPORT
     CyFxUvcAppPTZInit ();
 #endif
-
-    clearFeatureRqtReceived = CyFalse;
 
     /* Initialize the P-port. */
     pibclock.clkDiv      = 2;
